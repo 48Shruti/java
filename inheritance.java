0d0f07898e16105ddb0dce3981588ad02e5a8569
Shruti sharma <105702669+48Shruti@users.noreply.github.com>
2023-10-22 17:54:31 +0530

    inheritance

---
+++ inheritance.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+//Single inhertance
+// class base {
+//     String name = new String("Shruti");
+// }
+
+// public class inheritance extends base {
+//     public static void main(String[] args) {
+//         inheritance obj = new inheritance();
+//         System.out.println(obj.name);
+//     }
+// }
+
+//Multiple inheritance
+class A {
+    Scanner scan = new Scanner(System.in);
+
+}
+
+class inheritance {
+    public static void main(String[]args){
+        inheritance multiobj = new inheritance()
+        System.out.println("Enter input ");
+        String input = nextLine();
+    }
+}
