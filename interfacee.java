d43aafdf9344d6b5f16025c2898feffe77a9e027
Shruti sharma <105702669+48Shruti@users.noreply.github.com>
2023-10-26 21:55:32 +0530

    updates

--- interfacee.java
+++ interfacee.java
@@ -1,12 +1,21 @@
 interface hello {
     public void show();
 
+    public int add();
+
     public void display();
 }
 
 public class interfacee implements hello {
     public void show() {
         System.out.println("Show");
+    }
+
+    public int add() {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        return c;
     }
 
     public void display() {
@@ -17,5 +26,6 @@ public class interfacee implements hello {
         interfacee int1 = new interfacee();
         int1.show();
         int1.display();
+        int1.add();
     }
 }
