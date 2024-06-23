4213cae98b83d26186bf6ba22d5fe760b475b5fc
Shruti sharma <105702669+48Shruti@users.noreply.github.com>
2024-06-23 21:02:05 +0530

    basic problem

---
+++ dailyQuestion/evenOdd.java
@@ -0,0 +1,18 @@
+
+package dailyQuestion;
+
+import java.util.*;
+
+public class evenOdd {
+    // scanner for input
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter input");
+        int inp = sc.nextInt();
+        if (inp % 2 == 0) {
+            System.out.println("input is even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+}
---
+++ dailyQuestion/swapTwoNum.java
@@ -0,0 +1,25 @@
+package dailyQuestion;
+
+import java.util.Scanner;
+
+public class swapTwoNum {
+    public void swap(int num1, int num2) {
+        int temp = 0;
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+        System.out.println("output");
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter inp1");
+        int int1 = sc.nextInt();
+        System.out.println("Enter inp2");
+        int int2 = sc.nextInt();
+        swapTwoNum swap1 = new swapTwoNum();
+        swap1.swap(int1, int2);
+    }
+}
